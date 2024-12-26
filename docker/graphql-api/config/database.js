e97dfae286db9e2ca2a540dbf31193c92957c6b5
ecastill05 <154347846+ecastill05@users.noreply.github.com>
2024-12-26 08:18:01 -0500

    Delete docker directory
    Lo reemplazo para crear un directorio con más avances

--- docker/graphql-api/config/database.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-// Setup PostgreSQL connection
-const sequelize = new Sequelize('postgres://postgres:postgres@db:5432/metrobus_db', {
-  dialect: 'postgres',
-  logging: true, // Disable logging for cleaner output
-});
-
-module.exports = sequelize;
