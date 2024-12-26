27deb059a72bb1b470ff4adddde0863b233a5487
ecastill05 <154347846+ecastill05@users.noreply.github.com>
2024-12-26 08:50:11 -0500

    Create server.js

---
+++ prueba_tecnica/rest-api/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const alcaldiasRoutes = require('./routes/alcaldias');
+const stopsRoutes = require('./routes/stops');
+
+const app = express();
+const PORT = 3001;
+
+app.use(bodyParser.json());
+app.use('/api', alcaldiasRoutes);
+app.use('/api', stopsRoutes);
+
+app.listen(PORT, () => {
+  console.log(`REST API running on http://localhost:${PORT}`);
+});
